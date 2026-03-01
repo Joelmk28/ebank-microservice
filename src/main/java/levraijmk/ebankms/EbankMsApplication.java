8b1191a350c0d951d46f7b7bffbf68c10c607065
Joelmk28 <joelmuhindok@gmail.com>
2026-03-01 13:10:11 +0100

    Initial commit

---
+++ src/main/java/levraijmk/ebankms/EbankMsApplication.java
@@ -0,0 +1,13 @@
+package levraijmk.ebankms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankMsApplication.class, args);
+    }
+
+}
