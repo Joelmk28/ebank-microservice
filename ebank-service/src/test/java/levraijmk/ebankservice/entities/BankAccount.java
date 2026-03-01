abd32b0b306f0a91e676e4cd8acfa715e526922d
Joelmk28 <joelmuhindok@gmail.com>
2026-03-01 15:07:50 +0100

    création de la classe Customer

--- ebank-service/src/test/java/levraijmk/ebankservice/entities/BankAccount.java
+++ ebank-service/src/test/java/levraijmk/ebankservice/entities/BankAccount.java
@@ -3,6 +3,7 @@ package levraijmk.ebankservice.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +24,10 @@ public class BankAccount {
     private double balance;
 
     private String type;
-    private long customerId;
+    private long customerId;// clé entragere de la table customer dans le customer service
+
+
+    @Transient // pour que JPA ne le reprensete pas comme une Table dans la bd
+    private Customer customer;
+
 }
