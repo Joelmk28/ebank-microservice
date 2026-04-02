8ae7fbe7cce89b14c83bceddc5c3e4763c83a71e
Joelmk28 <joelmuhindok@gmail.com>
2026-04-02 17:12:51 +0100

    configuration serveur pour traiter les applications.properties des sevices dans un seul dossier

---
+++ customer-service/src/main/java/levraijmk/customerservice/TestConfigController.java
@@ -0,0 +1,19 @@
+package levraijmk.customerservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope // <--- Permet de rafraîchir la valeur sans redémarrer (optionnel)
+public class TestConfigController {
+
+    @Value("${global.params.message}") // Récupère la valeur du Config Server
+    private String message;
+
+    @GetMapping("/test-config")
+    public String test() {
+        return "Message reçu : " + message;
+    }
+}
